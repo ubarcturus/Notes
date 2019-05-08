0b487db4e7538eab2621af0486f9d179d62d7c89
ubarcturus <ubarcturus@hotmail.com>
2019-05-08 16:09:06 +0200

    add burger too page "add"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    /*{
+    { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+/*    {
         path: '',
         redirectTo: 'home',
         pathMatch: 'full'
     },*/
-    { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-    /*  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-        { path: 'list', loadChildren: './list/list.module#ListPageModule' },
-        { path: 'add', loadChildren: './add/add.module#AddPageModule' }*/
+    { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+    { path: 'list', loadChildren: './list/list.module#ListPageModule' },
+    { path: 'add', loadChildren: './add/add.module#AddPageModule' }
 ];
 
 @NgModule({
