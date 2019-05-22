4515bd12c9d551177843ad546fe2267b5a069bab
ubarcturus <ubarcturus@hotmail.com>
2019-05-22 08:49:50 +0200

    Cordova

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
     selector: 'app-add',
@@ -11,4 +13,30 @@ export class AddPage implements OnInit {
 
     ngOnInit() {}
 
+}
+
+// camera.service.ts
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  constructor(private camera: Camera) { }
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // Do something with the new photo
+
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue: " + err);
+    });
+  }
 }
