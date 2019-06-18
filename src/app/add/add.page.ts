5e4972b37a48f61b8622b40901f832450e6e6558
ubarcturus <ubarcturus@hotmail.com>
2019-06-18 16:13:32 +0200

    slim camera function and add gallery

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -15,7 +15,7 @@ export class AddPage implements OnInit {
 
     constructor(private camera: Camera) {}
 
-    foto() {
+    foto(source) {
         const options: CameraOptions = { // Einstellungen für die Kamera: https://github.com/apache/cordova-plugin-camera#cameracameraoptions--object
             sourceType: 1, // Quelle des Bildes (Fotogalerie, 0), (Kamera, 1), (Fotoordner, 2)
             quality: 100, // Die Qualität gibt an, wie scharf das Bild ist. Bis Stufe 70 sollte man keine Änderungen wahrnehmen können
@@ -23,15 +23,37 @@ export class AddPage implements OnInit {
             encodingType: 0, // Zielformat des Bildes (JPEG, 0), (PNG, 1)
             mediaType: 2, // Nur wenn sourceType 0 oder 2: Angezeigte Dateitypen: Bild (PICTURE, 0), Video (VIDEO, 1), frei (ALLMEDIA, 2)
             saveToPhotoAlbum: true, // ob das erstellte Bild in die Galerie gespeichert werden soll
-            allowEdit: true, // ob das erstellte Bild editiert werden soll
+            // allowEdit: true, // ob das erstellte Bild editiert werden soll
             correctOrientation: true, //dreht das Bild, dass es richtig herum angezeigt wird
             // targetHeight: 200, // Die Höhe des Bildes in Pixel
             // targetWidth: 200 // Die Breite des Bildes in Pixel
         }
+        if (source == 'gallery') {
+        	options.sourceType = 0;
+        	options.saveToPhotoAlbum = false;
+        }
+
         this.camera.getPicture(options).then(imageURI => { // Das Bild wird gemacht
             this.myphoto = imageURI;
         }, (err) => {
             console.log("Fehler bei der Kamera:", err); // Falls etwas schief geht, wird die Errormeldung ausgegeben
         });
     }
+
+    /*gallery(gallery) {
+    	const options: CameraOptions = { // Einstellungen für die Kamera: https://github.com/apache/cordova-plugin-camera#cameracameraoptions--object
+            sourceType: 0, // Quelle des Bildes (Fotogalerie, 0), (Kamera, 1), (Fotoordner, 2)
+            quality: 100, // Die Qualität gibt an, wie scharf das Bild ist. Bis Stufe 70 sollte man keine Änderungen wahrnehmen können
+            destinationType: 1, // Zieltyp des Bildes (DATA_URL, 0), (FILE_URI, 1), (NATIVE_URI, 2)
+            encodingType: 0, // Zielformat des Bildes (JPEG, 0), (PNG, 1)
+            mediaType: 2, // Nur wenn sourceType 0 oder 2: Angezeigte Dateitypen: Bild (PICTURE, 0), Video (VIDEO, 1), frei (ALLMEDIA, 2)
+            // saveToPhotoAlbum: true, // ob das erstellte Bild in die Galerie gespeichert werden soll
+            allowEdit: true, // ob das erstellte Bild editiert werden soll
+            correctOrientation: true, //dreht das Bild, dass es richtig herum angezeigt wird
+            // targetHeight: 200, // Die Höhe des Bildes in Pixel
+            // targetWidth: 200 // Die Breite des Bildes in Pixel
+        }
+        this.camera.getPicture(options).then(imageURI => { // Das Bild wird gemacht
+        	this.myphoto = imageURI;
+        })*/
 }
