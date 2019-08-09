cbd945ace1c7c128b19f8262873f688645c48064
ubarcturus <ubarcturus@gmail.com>
2019-08-09 15:43:56 +0200

    more sort

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -18,22 +18,25 @@ export class ListPage implements OnInit {
 
     constructor(
         public storage: Storage,
-        sortedNotes = null,
+        sortedNotes = [],
     ) {
 
         this.storage.get("allNotes").then((res) => {
             res.sort((a, b) => a.dateTime - b.dateTime);
-            sortedNotes = res;
+            sortedNotes = res.allNotes;
         });
+        for (var i = 0; i < sortedNotes.length; i++) {
+            sortedNotes[i]
+        }
 
 
-        for (let i = 1; i < 11; i++) {
+        /*for (let i = 1; i < 11; i++) {
             this.listItems.push({
                 title: 'Überschrift ' + i,
                 dateTime: Math.floor((Math.random() * 100) + 1),
                 content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error voluptatem, dolores explicabo aperiam facilis praesentium, deserunt, sit sed aut quod cupiditate. Porro recusandae ipsa dicta nihil voluptas neque, distinctio, iure? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum delectus, eius perferendis quam sed iure laudantium officiis. Tempore dolorum, debitis dolor itaque quis vero facilis facere quos sunt, officiis pariatur.",
             });
-        }
+        }*/
 
         /*this.storage.get("allNotes").then((res) => {
             if (res. noch keine Einträge) {
